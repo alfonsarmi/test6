b2577d7352187599a6c4e05eec6ad57cce7f4c48
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-06-11 08:30:59 +0200

    first commit

---
+++ testcognitivecomplexity.js
@@ -0,0 +1,26 @@
+
+var a = 1;
+var b = 1;
+if (a = 1)
+{
+	if (a = 1 && b == 2 || b == 1)
+	{
+		if (b = 2)
+		{
+			switch (b)
+			{
+				case 1:
+				break;
+				
+				case 2:
+				break;
+				
+				default:
+				break;
+				
+			}
+			
+		}
+	}
+	
+}
